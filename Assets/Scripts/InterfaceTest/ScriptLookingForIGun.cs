c1e897f1543ab86ee38cc2d5e725567c8cb418ca
andrew downie <andrew_dev@outlook.com>
2017-05-23 10:02:19 -0400

    I was not aware that GetComponet<Interface>() worked properly in Unity 5. I have a lot of thinking about refactoring to do. Now if only unity supported C# interfaces in the inspector, though I may be able to get by without the interfaces in the inspector.

---
+++ Assets/Scripts/InterfaceTest/ScriptLookingForIGun.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptLookingForIGun : MonoBehaviour {
+
+	void Start () {
+		IGun gun = gameObject.GetComponent<IGun>();
+
+		gun.Shoot();	
+	}
+
+}
