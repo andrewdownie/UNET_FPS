d9f4a72eff87148db71676f6bd48bbc9b916952e
andrew downie <andrew_dev@outlook.com>
2017-06-19 03:03:52 -0400

    Added scroll view to each of the panels for class selection.

---
+++ Assets/PlayerCombatClassSelect.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerCombatClassSelect : MonoBehaviour {
+	[SerializeField]
+	Text selectedClassName;
+	[SerializeField]
+	Button breecherBtn, scoutBtn, survivalistBtn, riflemanBtn;
+	[SerializeField]
+	GameObject breecherPanel, scoutPanel, survivalistPanel, riflemanPanel;
+
+
+	void Start () {
+		breecherBtn.onClick.AddListener(ShowBreecher);
+		scoutBtn.onClick.AddListener(ShowScout);
+		survivalistBtn.onClick.AddListener(ShowSurvivalist);
+		riflemanBtn.onClick.AddListener(ShowRifleman);
+	}
+	
+	void Update () {
+		
+	}
+
+
+	void ShowBreecher(){
+		selectedClassName.text = "Breecher";
+		breecherPanel.SetActive(true);
+		scoutPanel.SetActive(false);
+		survivalistPanel.SetActive(false);
+		riflemanPanel.SetActive(false);
+	}
+	void ShowScout(){
+		selectedClassName.text = "Scout";
+		breecherPanel.SetActive(false);
+		scoutPanel.SetActive(true);
+		survivalistPanel.SetActive(false);
+		riflemanPanel.SetActive(false);
+	}
+	void ShowSurvivalist(){
+		selectedClassName.text = "Survivalist";
+		breecherPanel.SetActive(false);
+		scoutPanel.SetActive(false);
+		survivalistPanel.SetActive(true);
+		riflemanPanel.SetActive(false);
+	}
+	void ShowRifleman(){
+		selectedClassName.text = "Rifleman";
+		breecherPanel.SetActive(false);
+		scoutPanel.SetActive(false);
+		survivalistPanel.SetActive(false);
+		riflemanPanel.SetActive(true);
+	}
+
+}
